1d9032d1d21f3b0f60328f6596d9c65487fc9355
Mike Benowitz <mwbenowitz@gmail.com>
2019-12-09 09:54:28 -0500

    SFR-703 Make execution internal to classify process
    Previously the ResearchNow ETL pipeline loaded work records from the Classify endpoint and then subsequently fetched the instance records from the lookup API asynchronously. This was done to ensure speedy execution while the Classify service was reading from a kinesis stream and the concurrency was limited, meaning that fast execution at that stage was vital. The drawback to this was that it requires a doubling of the update operations in the database in order to first store the "stump" instance records from the Classify function and then the fuller instances from the lookup function.
    
    The Classify function has since been converted to read from a SQS queue, which allows a much greater degree of concurrency. This means that these invocations can be longer running with a much lower performance hit (if there is one at all). Because of this it should be possible to do the fetching of instance records synchronously, and returning these results as a unified object for persistence in the database. This reduces the number of lookups and write operations in the db, potentially resulting ina large performance gain. It also eliminates the possibility of race conditions occuring with the instance records.
    
    To do so this function will not read from a queue but be invoked via an API call from the Classify function (in a multithreaded invocation), this makes these changes. It makes no changes to the internal logic, but represents a major change in how the function is invoked and returns data. This returns an object that shares its formatting with the VIAF function, so consuming the response should be very straightforward and familiar.

--- service.py
+++ service.py
@@ -1,9 +1,6 @@
-import json
-import base64
-
-from helpers.errorHelpers import NoRecordsReceived, OCLCError, DataError
 from helpers.logHelpers import createLog
 
+from lib.outPutManager import OutputManager
 from lib.recordFetch import fetchData
 
 # Logger can be passed name of current module
@@ -15,43 +12,24 @@ def handler(event, context):
     """Method invoked by Lambda event. Verifies that records were received and,
     if so, passes them to be parsed"""
     logger.debug('Starting Lambda Execution')
-
-    records = event.get('Records')
-
-    if records is None:
-        logger.error('Records block is missing in Kinesis Event')
-        raise NoRecordsReceived('Records block missing', event)
-    elif len(records) < 1:
-        logger.error('Records block contains no records')
-        raise NoRecordsReceived('Records block empty', event)
-
-    results = parseRecords(records)
-
-    logger.info('Successfully invoked lambda')
-
-    # This return will be reflected in the CloudWatch logs
-    # but doesn't actually do anything
-    return results
-
-
-def parseRecords(records):
-    """Simple method to parse list of records and process each entry."""
-    logger.debug('Parsing Messages')
-    return [parseRecord(r) for r in records]
-
-
-def parseRecord(encodedRec):
-    """Parse an individual record. Verifies that an object was able to be
-    decoded from the input base64 encoded string and if so, hands this to the
-    enhancer method"""
     try:
-        record = json.loads(encodedRec['body'])
-        return fetchData(record)
-    except json.decoder.JSONDecodeError as jsonErr:
-        logger.error('Invalid JSON block recieved')
-        logger.error(jsonErr)
-        raise DataError('Malformed JSON block recieved from SQS')
-    except KeyError as err:
-        logger.error('Missing body attribute in SQS message')
-        logger.debug(err)
-        raise DataError('Body object missing from SQS message')
+        queryIdentifier = event['queryStringParameters']['identifier']
+        queryType = event['queryStringParameters']['type']
+    except KeyError:
+        # Return 400 error if these keys cannot be found
+        return OutputManager.formatResponse(
+            400,
+            {'message': 'GET query must include identifier and type'}
+        )
+
+    try:
+        return OutputManager.formatResponse(
+            200,
+            fetchData(queryIdentifier, queryType)
+        )
+    except Exception as err:
+        # Return 500 error code for a bad execution
+        return OutputManager.formatResponse(
+            500,
+            {'message': err.message}
+        )
